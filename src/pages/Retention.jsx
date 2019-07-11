443076f75b2385982b3b6c583af4b8232667c3cf
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-07-11 19:38:30 +0700

    mock retention

--- src/pages/Retention.jsx
+++ src/pages/Retention.jsx
@@ -3,14 +3,31 @@ import { jsx, css } from '@emotion/core'
 import ReactCohortGraph from 'react-cohort-graph'
 
 import { Container } from '../components/Layout'
+import { LineChart } from '../components/Chart'
+import { Card } from '../components/Card'
 
 const DATA = {
   weeks: {
-      "week1": [200, 10, 20],
-      "week2": [300, 200],
-      "week3": [200]
+      "week1": [200, 70, 20, 18, 17, 16, 17, 17],
+      "week2": [300, 200, 150, 140, 80, 50, 40],
+      "week3": [200, 100, 50, 10, 8, 7]
   },
 }
+
+const lineData = [
+  {
+    name: 'week1',
+    data: [200, 70, 20, 18, 17, 16, 17, 17],
+  },
+  {
+    name: 'week2',
+    data: [300, 200, 150, 140, 80, 50, 40],
+  },
+  {
+    name: 'week3',
+    data: [200, 100, 50, 10, 8, 7]
+  }
+]
 
 const Retention = () => {
   return (
@@ -21,10 +38,37 @@ const Retention = () => {
         height: 100%;
       `}
     >
-      <ReactCohortGraph
-        data={DATA}
-        showHeaderValues
-      />
+      <Container
+        margin='0'
+        padding='0'
+        justify='center'
+        align='center'
+      >
+        <Card
+          css={css`
+            width: 50%;
+          `}
+        >
+          <LineChart
+            data={lineData}
+            style={{
+              width: '100%',
+              height: '300px'
+            }}
+          />
+        </Card>
+        <div
+          css={css`
+            width: 80%;
+            overflow: scroll;
+          `}
+        >
+          <ReactCohortGraph
+            data={DATA}
+            showHeaderValues
+          />
+        </div>
+      </Container>
     </Container>
   )
 }
