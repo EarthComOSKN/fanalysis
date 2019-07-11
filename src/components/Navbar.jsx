19425821ef9c8bec5ecf42a84f6be3089aca3605
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-07-11 11:49:15 +0700

    set structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,27 @@
 /** @jsx jsx */
-import { css, jsx } from "@emotion/core";
-import { Button ,Icon} from 'antd';
-import { NavbarContainer,Title, Container } from "./Layout";
+import { css, jsx } from '@emotion/core'
+import { Button } from 'antd'
+import { NavbarContainer, Title, Container } from './Layout'
 
-
-const Navbar = () => {
-  return <NavbarContainer >
-    <Title css={css `margin-left:2rem;`}>Fanalysis</Title>
-    <Container css={css`justify-content:flex-end`} row>
-      <Button >Overview</Button>
-      <Button >Overview</Button>
+const Navbar = () => (
+  <NavbarContainer>
+    <Title
+      css={css`
+        margin-left: 2rem;
+      `}
+    >
+      Fanalysis
+    </Title>
+    <Container
+      css={css`
+        justify-content: flex-end;
+      `}
+      row
+    >
+      <Button>Overview</Button>
+      <Button>Overview</Button>
     </Container>
   </NavbarContainer>
-};
+)
 
 export default Navbar
