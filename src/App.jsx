c6a0799ba4d2fec79c7dbd3734bd27340f3bc3ac
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-07-11 11:49:45 +0700

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/core'
+import { jsx, css } from '@emotion/core'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Navbar from './components/Navbar'
+import { Container } from './components/Layout'
 
 import Overview from './pages/Overview'
 
 const App = () => {
   return (
-    <Router>
-      <Route
-        path='/'
-        exct
-        component={Overview}
-      />
-    </Router>
+    <Container
+      margin='0'
+      padding='0'
+      css={css`
+        height: 100vh;
+        width: 100vw;
+      `}
+    >
+      <Navbar />
+      <Router>
+        <Route
+          path='/'
+          exct
+          component={Overview}
+        />
+      </Router>
+    </Container>
   )
 }
 
