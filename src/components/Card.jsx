c1c27c1a2bb7f6616f4c612c877850d500e584ad
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-07-11 22:20:08 +0700

    fix chart size

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -32,6 +32,8 @@ const CardContent = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  width: 100%;
+  height: 100%;
 `;
 
 export const Card = ({ title, style, children }) => {
