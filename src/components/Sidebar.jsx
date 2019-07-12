a52804c50d8fc9986185c57f14c0227ff2bf11ad
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-07-12 10:12:36 +0700

    remove duration label

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -177,29 +177,6 @@ const Panal = props => {
           RETENTION
         </div>
       </MyButton>
-      <MyButton
-        active={window.location.pathname === "/duration"}
-        onClick={() => {
-          props.history.push("/duration");
-        }}
-      >
-        <TimeIcon
-          css={css`
-            width: 30px;
-            height: 30px;
-          `}
-        />
-        <div
-          css={css`
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            margin-left: 1rem;
-          `}
-        >
-          DURATION
-        </div>
-      </MyButton>
     </div>
   );
 };
