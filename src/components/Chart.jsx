f14229c9a3f2b045fc79d74085179d4c6931df68
Puripat Arayasirikul <boat@Puripats-MacBook-Pro.local>
2019-07-12 10:51:14 +0700

    add label

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -249,14 +249,14 @@ export const GenderTimeChart = () => {
     textStyle: {
       color: "white"
     },
-    title: {
-      text: "折线图堆叠"
-    },
     tooltip: {
       trigger: "axis"
     },
     legend: {
-      data: ["邮件营销", "联盟广告", "视频广告", "直接访问", "搜索引擎"]
+      data: ["old", "new", 'transaction'],
+      textStyle: {
+        color: '#fff'
+      }
     },
     grid: {
       left: "3%",
@@ -271,42 +271,31 @@ export const GenderTimeChart = () => {
     },
     xAxis: {
       type: "category",
-      boundaryGap: false,
-      data: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"]
+      data: ["10:00", "11:00", "12:00", "13:00", '14:00', '15:00', '16:00', '17:00', '18:00']
     },
     yAxis: {
       type: "value"
     },
     series: [
       {
-        name: "邮件营销",
-        type: "line",
-        stack: "总量",
-        data: [120, 132, 101, 134, 90, 230, 210]
+        name: "old",
+        type: "bar",
+        stack: 'gender',
+        data: [20, 100, 80, 10, 50, 70, 120, 40, 130]
       },
       {
-        name: "联盟广告",
-        type: "line",
-        stack: "总量",
-        data: [220, 182, 191, 234, 290, 330, 310]
+        name: "new",
+        type: "bar",
+        stack: "gender",
+        data: [5, 20, 15, 50, 120, 100, 90, 75, 30]
       },
       {
-        name: "视频广告",
-        type: "line",
-        stack: "总量",
-        data: [150, 232, 201, 154, 190, 330, 410]
-      },
-      {
-        name: "直接访问",
-        type: "line",
-        stack: "总量",
-        data: [320, 332, 301, 334, 390, 330, 320]
-      },
-      {
-        name: "搜索引擎",
-        type: "line",
-        stack: "总量",
-        data: [820, 932, 901, 934, 1290, 1330, 1320]
+        name: 'transaction',
+        type: 'line',
+        data: [20, 95, 70, 30, 100, 105, 80, 50, 120],
+        itemStyle: {
+          color: 'yellow'
+        }
       }
     ]
   };
@@ -359,7 +348,7 @@ export const DurationChart = () => {
         name: "Avg Time Spend",
         type: "bar",
         barWidth: "60%",
-        data: [69, 220, 160, 434, 120]
+        data: [69, 220, 160, 434, 120],
       }
     ]
   };
